0abf6bd91ec1bcff60991242471d831736de687b
Diaby Mamadou <85366150+404diaby@users.noreply.github.com>
2024-12-16 06:32:09 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 //redirection sur le HeroSection
-if(window.location.href != 'diaby-mamadou.vercel.app/index.html#heroSection'){
-    window.location.href = 'diaby-mamadou.vercel.app/index.html#heroSection';
+if(window.location.href != 'https://diaby-mamadou.vercel.app/index.html#heroSection'){
+    window.location.href = 'https://diaby-mamadou.vercel.app/index.html#heroSection';
 }
 
 
